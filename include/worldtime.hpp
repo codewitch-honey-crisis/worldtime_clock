b0e276686eb6bdd6904d9f55d88b553159e31e96
honey the codewitch <gaztoast@gmail.com>
2022-06-10 07:26:24 -0700

    simplified

--- include/worldtime.hpp
+++ include/worldtime.hpp
@@ -3,7 +3,6 @@
 struct worldtime final {
 #ifdef ESP32
     static time_t now(int8_t utc_offset);
-    static time_t now(const char* timezone = nullptr);
 #endif
     static time_t parse(Stream& stm);
 };
--- src/worldtime.cpp
+++ src/worldtime.cpp
@@ -8,25 +8,17 @@
 #include <worldtime.hpp>
 #ifdef ESP32
 time_t worldtime::now(int8_t utc_offset) {
-    long long result = now("Etc/UTC");
-    return (time_t)result+(utc_offset*3600);
-}
-time_t worldtime::now(const char* timezone) {
+    constexpr static const char* url = "http://worldtimeapi.org/api/timezone/Etc/UTC";
     HTTPClient client;
-    char url[512];
-    if(timezone==nullptr) {
-        strcpy(url,"http://worldtimeapi.org/api/ip");
-    } else {
-        sprintf(url,"http://worldtimeapi.org/api/timezone/%s",timezone);
-    }
     client.begin(url);
     if(0>=client.GET()) {
         return time_t(0);
     }
     time_t result = parse(client.getStream());
     client.end();
-    return result;
+    return (time_t)result+(utc_offset*3600);
 }
+
 #endif
 time_t worldtime::parse(Stream& stm) {
     if(!stm.available()) {
